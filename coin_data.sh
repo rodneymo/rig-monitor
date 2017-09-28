8b7ae8f4389669a2609c14dc8520672a3e795e7f
rodneymo <rodney.peo@gmail.com>
2017-09-28 18:02:52 +0200

    few issues fixed in coin_data script

--- coin_data.sh
+++ coin_data.sh
@@ -9,23 +9,21 @@ cd $BASE_DIR
 # epoch RUN_TIME
 RUN_TIME=`date +%s`
 
-if [ -f ${BASE_DIR}/run/COINMARKET_LOCK ]; then
+if [ -f ${BASE_DIR}/run/COIN_LOCK ]; then
     	echo "coinmarket process still running! Exiting..."
 	exit
 else
-	touch  ${BASE_DIR}/run/COINMARKET_LOCK
+	touch  ${BASE_DIR}/run/COIN_LOCK
 fi
 
 for ARGUMENT in "$@"; do
         if [ "$ARGUMENT" == "-trace" ]; then
                 set -x
-        elif [[ $ARGUMENT =~ ^-c[0-9]+ ]]; then
+        elif [ "$ARGUMENT" == "-debug" ]; then
                 DEBUG=1
-                L_INDEX=${ARGUMENT:2}
-                COIN_LIST=("${COIN_LIST[@]:$L_INDEX:1}")
         else
                 echo "Argument unknonw: ${ARGUMENT}"
-		rm ${BASE_DIR}/run/COINMARKET_LOCK 
+		rm ${BASE_DIR}/run/COIN_LOCK 
 		exit
         fi
 done
@@ -35,76 +33,63 @@ SAVEIFS=$IFS
 
 ############# query crypto netwwork info from whattomine #############
 
-WHATTOMINE_URL="https://whattomine.com/coins.json"
-
-WHATTOMINE_OUTPUT=`curl -s "${WHATTOMINE_URL}" | jq -r '.'`
+WTM_URL="https://whattomine.com/coins.json"
+WTM_OUTPUT=`curl -s -m 10 "${WTM_URL}" | jq -r '.'`
 if (( DEBUG == 1 )); then
-	echo $WHATTOMINE_URL
+	echo "whattomine URL: ${WTM_URL}"
+	echo "$WTM_OUTPUT"
 fi
 
-API_STATUS=`echo $WHATTOMINE_OUTPUT | jq -r '.'`
-
-if [ "$API_STATUS" == "" ]; then
-        echo "WHATTOMINE website seems to be down"
+if [ "$WTM_OUTPUT" == "" ]; then
+        echo "whattomine.com seems to be down"
 fi
 
 ############# query coinmarketcap.com #############
-for COIN_LINE in "${COIN_LIST[@]}"
+
+CMC_URL="https://api.coinmarketcap.com/v1/ticker/"
+if [ "$QUOTE_CURRENCY" != "USD" ]; then
+	CMC_URL="${CMC_URL}?convert=${QUOTE_CURRENCY}"
+fi
+
+CMC_OUTPUT=`curl -s -m 10 "${CMC_URL}" | jq -r '.'`
+if (( DEBUG == 1 )); then
+	echo "coinmarketcap URL: ${CMC_URL}"
+	echo "$CMC_OUTPUT"
+fi
+
+if [ "$CMC_OUTPUT" == "" ]; then
+        echo "coinmarketcap seems to be down"
+fi
+
+############ parse and merge data ################
+for POOL_LINE in "${POOL_LIST[@]}"
 do
-	IFS=$',' read BASE_CURRENCY QUOTE_CURRENCY <<<${COIN_LINE}
-	if (( DEBUG == 1 )); then
-		echo $BASE_CURRENCY $QUOTE_CURRENCY
-	fi
-
-	COIN_URL="https://api.coinmarketcap.com/v1/ticker/${BASE_CURRENCY}/"
-	if [ "$QUOTE_CURRENCY" != "USD" ]; then
-		COIN_URL="${COIN_URL}?convert=${QUOTE_CURRENCY}"
-	fi
+	IFS=$',' read POOL_TYPE CRYPTO LABEL BASE_API_URL API_TOKEN WALLET_ADDR <<<${POOL_LINE}
 
 	if (( DEBUG == 1 )); then
-		echo $COIN_URL
+		echo "Looking up network info for ${CRYPTO}/${QUOTE_CURRENCY}"
 	fi
 
-	CURL_OUTPUT=`curl -s "${COIN_URL}" | jq -r '.'`
-	if (( DEBUG == 1 )); then
-		echo $CURL_OUTPUT
-	fi
+	MEASUREMENT="coin_data"
+	TAGS="crypto_name=${CRYPTO},quote_currency=${QUOTE_CURRENCY}"
+	WTM_FIELDS=`echo $WTM_OUTPUT | jq -r --arg crypto $CRYPTO '.coins | to_entries[] | select (.value.tag==$crypto) | "difficulty=\(.value.difficulty),block_reward=\(.value.block_reward),block_time=\(.value.block_time)"' | sed 's/null/0/g' `
 
-        CURL_STATUS=`echo $CURL_OUTPUT | jq -r '.error?'`
-	if (( DEBUG == 1 )); then
-		echo $CURL_STATUS
-	fi
-        if [ "$CURL_STATUS" == "id not found" ]; then
-                echo "$BASE_CURRENCY DOES NOT EXIST. PLEASE CHECK CONF FILE"
-		continue
-	fi
-
-	MEASUREMENT="coinmarketcap"
-	CRYPTO_SYMBOL=`echo $CURL_OUTPUT | jq -r '.[].symbol'` 
-	TAGS="crypto_name=${BASE_CURRENCY},base_currency=${CRYPTO_SYMBOL},quote_currency=${QUOTE_CURRENCY}"
-	if (( DEBUG == 1 )); then
-		echo "Looking up network info for ${BASE_CURRENCY}(${CRYPTO_SYMBOL})"
-	fi
-
-	WHATTOMINE_FIELDS=`echo $WHATTOMINE_OUTPUT | jq -r --arg crypto $CRYPTO_SYMBOL '.coins | to_entries[] | select (.value.tag==$crypto) | "difficulty=\(.value.difficulty),block_reward=\(.value.block_reward),block_time=\(.value.block_time)"' | sed 's/null/0/g' `
-	if [ "${WHATTOMINE_FIELDS}" != "" ]; then
-		WHATTOMINE_FIELDS=",${WHATTOMINE_FIELDS}"
-	fi
-	if (( DEBUG == 1 )); then
-		echo $WHATTOMINE_FIELDS
-	fi
 
 	if [ "$QUOTE_CURRENCY" != "USD" ]; then
 		PRICE="price_${QUOTE_CURRENCY,,}"
 		VOLUME="24h_volume_${QUOTE_CURRENCY,,}"
 		MARKET="market_cap_${QUOTE_CURRENCY,,}"
+		FIELDS=`echo $CMC_OUTPUT | jq -r --arg crypto $CRYPTO --arg price $PRICE --arg volume $VOLUME --arg market $MARKET --arg currency $QUOTE_CURRENCY '.[] | select (.rank=="$crypto") | "\($price)=\(.[$price]),price_btc=\(.price_btc),\($volume)=\(.[$volume]),\($market)=\(.[$market])"' `
 
-		FIELDS=`echo $CURL_OUTPUT | jq -r --arg price $PRICE --arg volume $VOLUME --arg market $MARKET --arg currency $QUOTE_CURRENCY '.[] | "\($price)=\(.[$price]),price_btc=\(.price_btc),\($volume)=\(.[$volume]),\($market)=\(.[$market])"' `
 	else
 		FIELDS=`echo $CURL_OUTPUT | jq -r '.[] | "price_usd=\(.price_usd),price_btc=\(.price_btc),24h_volume_usd=\(."24h_volume_usd"),market_cap_usd=\(.market_cap_usd)"'  `
 
 	fi
-	LINE="${MEASUREMENT},${TAGS} ${FIELDS}${WHATTOMINE_FIELDS}"
+	LINE="${MEASUREMENT},${TAGS} ${FIELDS}${WTM_FIELDS}"
+	LINE=`echo $LINE | sed -e 's/,$//g' `
+	if (( DEBUG == 1 )); then
+		echo "${LINE}"
+	fi
 	DATA_BINARY="${DATA_BINARY}"$'\n'"${LINE}"
 
 done
@@ -115,5 +100,5 @@ fi
 curl -i -XPOST 'http://localhost:8086/write?db=rigdata' --data-binary "${DATA_BINARY}"
 
 IFS=$SAVEIFS
-rm ${BASE_DIR}/run/COINMARKET_LOCK 
+rm ${BASE_DIR}/run/COIN_LOCK 
 
